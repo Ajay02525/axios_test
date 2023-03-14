910fbfe9dc75d7e6ce5a74eb268fc107db4f7fbc
Ajay02525 <103028175+Ajay02525@users.noreply.github.com>
2023-03-14 11:19:14 +0530

    Add files via upload

---
+++ route_axios/config.js
@@ -0,0 +1,12 @@
+const config = {
+    port:3306,
+    mysql:{
+        user:'root',
+        password:"Admin@123",
+        database:"db_axios",
+        host:"localhost"
+    }
+
+}
+
+module.exports = config;
